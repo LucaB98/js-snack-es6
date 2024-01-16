6f2e9cfc288ea6d45711e729dbe7cc66679e6911
LucaB98 <lucabellan98@gmail.com>
2024-01-16 14:39:56 +0100

    added list students

--- js/script.js
+++ js/script.js
@@ -1,27 +1,40 @@
-// SNACK 1
-// TAVOLO VIP
-const tableName = 'Tavolo Vip';
-// LISTA DEGLI INVITATI
-const guests = [
-  'Brad Pitt',
-  'Johnny Depp',
-  'Lady Gaga',
-  'Cristiano Ronaldo',
-  'Georgina Rodriguez',
-  'Chiara Ferragni',
-  'Fedez',
-  'George Clooney',
-  'Amal Clooney',
-  'Maneskin'
+// SNACK 2
+// ELENCO DEGLI STUDENTI
+const studentList = [
+    {
+        id: 213,
+        name: 'Marco della Rovere',
+        grades:78
+    },
+    {
+        id: 110,
+        name: 'Paola Cortellessa',
+        grades: 96
+    },
+    {
+        id: 250,
+        name: 'Andrea Mantegna',
+        grades: 48
+    },
+    {
+        id: 145,
+        name: 'Gaia Borromini',
+        grades: 74
+    },
+    {
+        id: 196,
+        name: 'Luigi Grimaldello',
+        grades: 68
+    },
+    {
+        id: 102,
+        name: 'Piero della Francesca',
+        grades: 50
+    },
+    {
+        id: 120,
+        name: 'Francesca da Polenta',
+        grades: 84
+    },
+
 ];
-
-
-const guestsList = guests.map((guest, index) => {
- return  {
-        table: tableName,
-        name: guest,
-        seatNumber: index + 1
-    };
-});
-
-console.log(guestsList)
