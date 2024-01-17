7d7f33a90c73332a13580dcc3d6f92acc2d7d86a
LucaB98 <lucabellan98@gmail.com>
2024-01-17 18:32:30 +0100

    finish snack-3

--- js/script.js
+++ js/script.js
@@ -2,27 +2,32 @@
 
 // CREO UN ELENCO DI BICI
 
-const bikeList = [
+const bikesList = [
     {
         name: 'bikered',
         weight: 13,
     },
     {
         name: 'bikeblu',
-        wheight: 10,
+        weight: 10,
     },
     {
         name:'bikegreen',
-        wheight: 15,
+        weight: 15,
     },
     {
         name:'bikeyellow',
-        wheight: 12,
+        weight: 12,
     },
     {
         name:'bikeblack',
-        wheight: 20,
+        weight: 20,
     }
 ];
 
 
+const {name: minWeightBikeName, weight: minWeight} = bikesList.reduce((min, bike) => (bike.weight < min.weight ? bike : min), bikesList[0]);
+
+console.log(`La bici più leggera è ${minWeightBikeName} con un peso di ${minWeight} kg`);
+
+     
