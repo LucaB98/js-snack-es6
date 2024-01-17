4df3152013726bc7f732a9541af192e1d066a9dd
LucaB98 <lucabellan98@gmail.com>
2024-01-17 18:13:42 +0100

    add list bike

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// SNACK 3
+
+// CREO UN ELENCO DI BICI
+
+const bikeList = [
+    {
+        name: 'bikered',
+        weight: 13,
+    },
+    {
+        name: 'bikeblu',
+        wheight: 10,
+    },
+    {
+        name:'bikegreen',
+        wheight: 15,
+    },
+    {
+        name:'bikeyellow',
+        wheight: 12,
+    },
+    {
+        name:'bikeblack',
+        wheight: 20,
+    }
+];
+
+
